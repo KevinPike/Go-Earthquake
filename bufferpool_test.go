51549272b188b3493f3d053adb69f6fe69b143cc
Kevin Pike <kevin.e.pike@gmail.com>
2014-06-04 08:35:07 -0400

    adds simple buffer pool test

---
+++ bufferpool_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestGet(t *testing.T) {
+	bufferPool := NewBufferPool("./Test100.dat", 1)
+	defer bufferPool.Shutdown()
+
+	rtn := make([]byte, 4)
+	bufferPool.Get(&rtn, 0)
+	bufferPool.Get(&rtn, 1024)
+}
